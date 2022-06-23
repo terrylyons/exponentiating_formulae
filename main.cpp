32ec0a0ed9cef38d7474cece9cd338447abddb5d
terrylyons <tlyons@maths.ox.ac.uk>
2022-06-24 00:57:25 +0100

    Added the biderectional transformer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 
 int forward_transformer();
 int adjoint_multiplication();
+int bidirectional_transformer();
+
 
 int main()
 {
-    forward_transformer();
-    adjoint_multiplication();
+//    forward_transformer();
+//    adjoint_multiplication();
+    bidirectional_transformer();
     return 0;
 }
